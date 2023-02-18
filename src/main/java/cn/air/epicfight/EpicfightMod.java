1dfb43ec670aef7844f1742a57bedd7e53266368
demaracia <1765441949@qq.com>
2023-02-19 03:07:53 +0800

    初始化项目配置

--- src/main/java/cn/air/epicfight/EpicfightMod.java
+++ src/main/java/cn/air/epicfight/EpicfightMod.java
@@ -1,14 +1,14 @@
-package net.fabricmc.example;
+package cn.air.epicfight;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class EpicfightMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("epicfight");
 
 	@Override
 	public void onInitialize() {
